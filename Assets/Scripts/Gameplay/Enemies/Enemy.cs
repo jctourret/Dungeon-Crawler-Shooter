460d19baaab64800ca3c68b0fbe30cc9bd478921
fermingimpel <fermingimpel@gmail.com>
2022-02-09 12:32:26 -0300

    Added basic pathfinding of enemies

--- Assets/Scripts/Gameplay/Enemies/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemies/Enemy.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour
 {
-
+    [SerializeField] protected AIPath pathfinding;
     [SerializeField] protected float health;
     [SerializeField] protected float damage;
     [SerializeField] protected float attackCooldown;
-
+    protected PlayerController player;
     protected virtual void Start()
     {
-        
+        player = FindObjectOfType<PlayerController>();
+        pathfinding.destination = player.transform.position;
     }
 
     protected virtual void Update()
